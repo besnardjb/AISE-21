fa586ec2a434d64eeaba706822bb154f32e13e62
Besnard Jean-Baptiste <jbbesnard@paratools.com>
2021-01-19 17:06:46 +0100

    FIND & KILL

---
+++ C1-generalites/TP1-corrections/builtin/kill.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+		return 0;
+
+
+	kill(atoi(argv[1]), SIGINT);
+	
+
+	return 0;
+}
