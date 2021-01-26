a4c3cbbe8b652fe127a56029646dbf69871a17c1
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2021-01-26 10:21:22 +0100

    EX4

--- C2-IOIPC/exemples_io/ex4_rw.c
+++ C2-IOIPC/exemples_io/ex4_rw.c
@@ -3,10 +3,36 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <errno.h>
 
-int main(int argc, char ** argv)
+ssize_t safe_write(int fd, void *buff,  size_t size)
 {
-	printf("%s\n", argv[0]);
+	size_t written = 0;
+	while( (size - written) != 0 )
+	{
+		errno = 0;
+		ssize_t ret = write(fd, buff + written, size-written);
+
+		if( ret < 0 )
+		{
+			if(errno == EINTR)
+			{
+				continue;
+			}
+
+			perror("write");
+			return ret;
+		}
+
+		written += ret;
+	}
+
+	return 0;
+}	
+
+
+
+int main(int argc, char ** argv){
 
 	if( argc != 2 )
 	{
@@ -22,7 +48,7 @@ int main(int argc, char ** argv)
 		return 1;
 	}
 
-	int cnt;
+	ssize_t cnt;
 	char buff[500];
 
 	while( (cnt = read(fd, buff, 500)) != 0)
@@ -33,7 +59,7 @@ int main(int argc, char ** argv)
 			return 1;
 		}
 
-		int ret = write(STDOUT_FILENO, buff, cnt);
+		int ret = safe_write(STDOUT_FILENO, buff, cnt);
 
 		if( ret < 0)
 		{
