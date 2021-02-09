2f74ff276cf562523988eeffba2be7c9ddf69f75
Julien Adam <adamj@paratools.com>
2021-02-09 17:05:12 +0100

    C4 - Mémoire : exo4

---
+++ C4-Memoire/exo4/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main() {
+  int var, check = 0x04030201;
+  char buf[40];
+
+  fgets(buf,50,stdin);
+
+  printf("check = %x\n", check);
+  if (check == 0xdeadbeef) {
+       printf("You Win !\n");
+     }
+   return 0;
+}
+
