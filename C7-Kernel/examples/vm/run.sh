56b6991e72ff2db280c7b65cf4319f7d522ececc
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2021-03-09 13:33:38 +0100

    Add vm run

---
+++ C7-Kernel/examples/vm/run.sh
@@ -0,0 +1,5 @@
+
+#qemu-system-x86_64 -drive file=${PWD}/img.qcow2,if=virtio -kernel /home/jbbesnard/repo/linux/./arch/x86/boot/bzImage  -append "console=ttyS0 debug rootfstype=ext4,xfs root=/dev/vda1 ide_core.noprobe=0.1 ide_core.noprobe=1.1"  --enable-kvm -cpu host -nographic
+
+qemu-system-x86_64 -drive file=${PWD}/img.qcow2,if=virtio -kernel /home/jbbesnard/repo/linux/./arch/x86/boot/bzImage --enable-kvm -cpu host -append "root=/dev/vda1" 
+
